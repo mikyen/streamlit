b739a9d96bb83c3870eb2219ad6f8022ebf2ba2f
Michele <mikyen86@gmail.com>
2024-01-19 14:22:12 +0100

    a

---
+++ the odds api/odds.py
@@ -0,0 +1,87 @@
+import argparse
+
+import requests
+
+
+# Obtain the api key that was passed in from the command line
+parser = argparse.ArgumentParser(description='Sample V4')
+parser.add_argument('--api-key', type=str, default='b83f2cd800664aae3826c88fb77164d6')
+args = parser.parse_args()
+
+
+# An api key is emailed to you when you sign up to a plan
+# Get a free API key at https://api.the-odds-api.com/
+API_KEY = args.api_key or 'YOUR_API_KEY'
+
+# Sport key
+# Find sport keys from the /sports endpoint below, or from https://the-odds-api.com/sports-odds-data/sports-apis.html
+# Alternatively use 'upcoming' to see the next 8 games across all sports
+SPORT = 'upcoming'
+
+# Bookmaker regions
+# uk | us | us2 | eu | au. Multiple can be specified if comma delimited.
+# More info at https://the-odds-api.com/sports-odds-data/bookmaker-apis.html
+REGIONS = 'eu'
+
+# Odds markets
+# h2h | spreads | totals. Multiple can be specified if comma delimited
+# More info at https://the-odds-api.com/sports-odds-data/betting-markets.html
+# Note only featured markets (h2h, spreads, totals) are available with the odds endpoint.
+MARKETS = 'h2h,spreads'
+
+# Odds format
+# decimal | american
+ODDS_FORMAT = 'decimal'
+
+# Date format
+# iso | unix
+DATE_FORMAT = 'iso'
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# First get a list of in-season sports
+#   The sport 'key' from the response can be used to get odds in the next request
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+sports_response = requests.get('https://api.the-odds-api.com/v4/sports', params={
+    'api_key': API_KEY
+})
+
+
+if sports_response.status_code != 200:
+    print(f'Failed to get sports: status_code {sports_response.status_code}, response body {sports_response.text}')
+
+else:
+    print('List of in season sports:', sports_response.json())
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# Now get a list of live & upcoming games for the sport you want, along with odds for different bookmakers
+# This will deduct from the usage quota
+# The usage quota cost = [number of markets specified] x [number of regions specified]
+# For examples of usage quota costs, see https://the-odds-api.com/liveapi/guides/v4/#usage-quota-costs
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+odds_response = requests.get(f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds', params={
+    'api_key': API_KEY,
+    'regions': REGIONS,
+    'markets': MARKETS,
+    'oddsFormat': ODDS_FORMAT,
+    'dateFormat': DATE_FORMAT,
+})
+
+if odds_response.status_code != 200:
+    print(f'Failed to get odds: status_code {odds_response.status_code}, response body {odds_response.text}')
+
+else:
+    odds_json = odds_response.json()
+    print('Number of events:', len(odds_json))
+    print(odds_json)
+
+    # Check the usage quota
+    print('Remaining requests', odds_response.headers['x-requests-remaining'])
+    print('Used requests', odds_response.headers['x-requests-used'])
